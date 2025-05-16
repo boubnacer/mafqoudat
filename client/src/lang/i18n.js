566a503b285243dc8e5b42cce65f518a9eb32e05
nacer <nassirox99@gmail.com>
2025-05-16 16:35:01 +0100

    Update related components for modern UI consistency

--- client/src/lang/i18n.js
+++ client/src/lang/i18n.js
@@ -11,6 +11,13 @@ i18n.use(initReactI18next).init({
         Lost: "Lost",
         All: "All",
         Returned: "Returned",
+        "Found Things": "Found Things",
+        "Lost Things": "Lost Things",
+        "Total Posts": "Total Posts",
+        "Returned to Owners": "Returned to Owners",
+        "new today": "new today",
+        "This month": "This month",
+        "Successfully returned": "Successfully returned"
       },
     },
     ar: {
@@ -20,6 +27,13 @@ i18n.use(initReactI18next).init({
         Lost: "فقد",
         All: "الكل",
         Returned: "رجع",
+        "Found Things": "الأشياء الموجودة",
+        "Lost Things": "الأشياء المفقودة",
+        "Total Posts": "إجمالي المنشورات",
+        "Returned to Owners": "تم إرجاعه لأصحابه",
+        "new today": "جديد اليوم",
+        "This month": "هذا الشهر",
+        "Successfully returned": "تم الإرجاع بنجاح"
       },
     },
   },
