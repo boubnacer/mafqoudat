92d5a9e48f7811e1697e601d39d1044f5c8ae711
nacer <nassirox99@gmail.com>
2025-08-12 01:00:07 +0100

    Temporarily allow all CORS origins for debugging

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -2,11 +2,15 @@ const allowedOrigins = require('./allowedOrigins')
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
+        // Allow all origins temporarily for debugging
+        callback(null, true)
+        
+        // Uncomment the following for production security:
+        // if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        //     callback(null, true)
+        // } else {
+        //     callback(new Error('Not allowed by CORS'))
+        // }
     },
     credentials: true,
     optionsSuccessStatus: 200
