1808298b1bad18a1ccce585b722bd7d43e2fa8cb
nacer <nassirox99@gmail.com>
2025-08-28 20:30:35 +0100

    debugging

--- generate-secrets.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-console.log('=== Generate JWT Secrets ===');
-console.log('JWT_SECRET:');
-console.log(crypto.randomBytes(64).toString('hex'));
-console.log('\nJWT_REFRESH_SECRET:');
-console.log(crypto.randomBytes(64).toString('hex'));
-console.log('\n=== Copy these values to your Railway environment variables ===');
-
