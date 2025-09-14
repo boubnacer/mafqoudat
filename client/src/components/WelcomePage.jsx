d8801627303dc8fa6fd7bff39e67892aea2210f0
nacer <nassirox99@gmail.com>
2025-09-14 21:14:34 +0100

    Update Welcome page

--- client/src/components/WelcomePage.jsx
+++ client/src/components/WelcomePage.jsx
@@ -170,7 +170,7 @@ const ActionButton = styled(IconButton)(({ theme }) => ({
 
 const TopRightContainer = styled(Box)(({ theme }) => ({
   position: 'absolute',
-  top: { xs: '24px', sm: theme?.spacing?.(2) || '16px' },
+  // top: { xs: '24px', sm: theme?.spacing?.(2) || '16px' },
   right: theme?.spacing?.(2) || '16px',
   zIndex: 10,
   marginTop: { xs: '8px', sm: '0px' },
