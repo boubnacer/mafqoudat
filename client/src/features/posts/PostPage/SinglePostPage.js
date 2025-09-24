6c368a4f8d58e63ddff88e7fbd5890d408ce3654
nacer <nassirox99@gmail.com>
2025-09-24 17:11:50 +0100

    fix the single post page essues

--- client/src/features/posts/PostPage/SinglePostPage.js
+++ client/src/features/posts/PostPage/SinglePostPage.js
@@ -677,7 +677,7 @@ const SinglePostPage = ({
                   position: 'absolute',
                   top: 16,
                   right: 16,
-                  zIndex: 5
+                  zIndex: 1
                 }}
               >
                 <Box
