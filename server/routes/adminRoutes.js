5780fb825c44d32a9068b58d82a91d3e2c51b782
nacer <nassirox99@gmail.com>
2025-10-09 18:48:01 +0100

    Backend - User Management APIs

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -17,6 +17,7 @@ const {
   deleteUserAdmin,
 } = require("../controllers/adminController");
 
+
 // All admin routes require authentication and admin role
 router.use(verifyJWT);
 router.use(verifyAdmin);
