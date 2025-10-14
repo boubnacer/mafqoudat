acc71083976edb1718ad0e36ce7c4992edf75831
nacer <nassirox99@gmail.com>
2025-10-14 21:23:23 +0100

    Update categories icons

--- client/src/config/categories.js
+++ client/src/config/categories.js
@@ -20,7 +20,6 @@ import {
 } from '@mui/icons-material';
 
 import DiamondOutlinedIcon from '@mui/icons-material/DiamondOutlined';
-import BusinessCenterOutlinedIcon from '@mui/icons-material/BusinessCenterOutlined';
 
 // Category configuration with Material UI icons - Optimized for Lost & Found
 export const CATEGORY_CONFIG = {
